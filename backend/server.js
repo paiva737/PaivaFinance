b8cb290e09a366437f63a4dc46c295d10da782c7
paiva737 <rafaelpaiva636@gmail.com>
2025-06-16 23:52:31 -0300

    testando comunicacao fullstack para envio do codigo

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const { enviarCodigo } = require('./services/emailService');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
+
 
 
 app.use(cors());
